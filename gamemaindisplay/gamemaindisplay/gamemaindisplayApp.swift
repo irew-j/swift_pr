b5f1cb1d7638caf9edd23c58b7a2be5b9599c028
irew-j <74749840+irew-j@users.noreply.github.com>
2023-12-29 21:18:43 +0900

    Add files via upload

---
+++ gamemaindisplay/gamemaindisplay/gamemaindisplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  gamemaindisplayApp.swift
+//  gamemaindisplay
+//
+//  Created by 김종웅 on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct gamemaindisplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
